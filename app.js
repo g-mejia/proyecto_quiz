b732a0b8e1aa479ccf562c0b47b6d5115ba4d9b5
Alvaro Gonzalez <alvaro.glez.mej@hotmail.com>
2015-05-04 21:59:29 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon(__dirname + '/public/favicon.ico')); //importar favicon
+app.use(favicon(__dirname + '/public/favicon.ico')); //se importa favicon de directorio public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
