c5ba53e0946c7d4146fb43c36ecc798d7d480e48
Alvaro Gonzalez <alvaro.glez.mej@hotmail.com>
2015-05-12 09:26:43 +0200

    Base de Datos2

--- models/models.js
+++ models/models.js
@@ -14,14 +14,14 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
 
 // sequelize.sync() crea e iniciliza tabla de preguntas en DB
-sequelize.sync().success(function() {
-	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function (count){
+sequelize.sync().then(function() {
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function (count){
 		if(count === 0) {   // la tabla se inicializa sólo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
 						})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
