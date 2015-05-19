d5fce5aab6e818ab2bee96db82b6ad1ae7efd154
Alvaro Gonzalez <alvaro.glez.mej@hotmail.com>
2015-05-19 17:12:05 +0200

    Autorización

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,23 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load);  //Autoload :quizId
 
 //Definicion de rutas de sesion
-router.get('/login', sessionController.new);  // formulario login
+router.get('/login',  sessionController.new);  // formulario login
 router.post('/login', sessionController.create);  // crear sesion
 router.get('/logout', sessionController.destroy);  // destruir sesion
 
 // Definicion de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes',                       quizController.index);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get('/quizes/new',                   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',               sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 
 //Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Autores', errors: [] });
